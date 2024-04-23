8577d1a76092fbb442849f84f7cf8d2689bbd40d
Best Damn Cat <bestdamncat@gmail.com>
2024-04-23 14:57:46 -0700

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
       <h1 className="text-center mt-5">The Purr-fect Revolution</h1>
       <h3>Unleash Your Inner Crypto-Cat with the Best Damn Cat Token!</h3>
 
+      <h5>CA: <a href="https://www.pump.fun/Gm8ZAUfqS1qr2PbXKceLhiTEzq9DzQmcfuWMX9MRqPS3">Gm8ZAUfqS1qr2PbXKceLhiTEzq9DzQmcfuWMX9MRqPS3</a></h5>
       <p>
         <a href="https://twitter.com/bestdamncat" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
           <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '1.2em', marginRight: '5px' }} />
