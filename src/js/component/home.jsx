4af5ef91d47d40556c87241592fb320970f9a1a3
Best Damn Cat <bestdamncat@gmail.com>
2024-04-23 14:15:31 -0700

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-
-//include images into your bundle
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faTelegram } from '@fortawesome/free-brands-svg-icons';
 import rigoImage from "../../img/rigo-baby.png";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">The Purr-fect Revolution</h1>
-			<h3>Unleash Your Inner Crypto-Cat with the Best Damn Cat Token!</h3>
-			<p>
-				<img src={rigoImage} />
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">The Purr-fect Revolution</h1>
+      <h3>Unleash Your Inner Crypto-Cat with the Best Damn Cat Token!</h3>
+
+      <p>
+        <a href="https://twitter.com/bestdamncat" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
+          <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '1.2em', marginRight: '5px' }} />
+        </a>	
+			
+        <a href="https://t.me/bestdamncat" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
+          <FontAwesomeIcon icon={faTelegram} style={{ fontSize: '1.2em', marginRight: '5px' }} />
+        </a>
+
+      </p>			
+      <p>
+        <img src={rigoImage} />
+      </p>
+    </div>
+  );
 };
 
 export default Home;
