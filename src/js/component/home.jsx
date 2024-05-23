a53c966684a46e54532344d310bb9b02420e25ad
Best Damn Cat <bestdamncat@gmail.com>
2024-05-22 22:11:45 -0700

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <h1 className="text-center mt-5">Buck Bundy</h1>
       <h3>I may be just a dog, but even I know that money is like a bone buried in the backyard: hard to find and never enough to go around!</h3>
 
-      <h5>CA: <a href="https://www.pump.fun/"></a></h5>
+      <h5>CA: <a href="https://www.pump.fun/DtdSYdz5VGUyv2WohK2msc9GXSPULY7MTU9aYQ2C2J2e">DtdSYdz5VGUyv2WohK2msc9GXSPULY7MTU9aYQ2C2J2e</a></h5>
       <p>
         <a href="https://twitter.com/buckbundysol" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
           <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '1.2em', marginRight: '5px' }} />
