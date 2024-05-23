5004df94a9553d85f94bb4b3b62c8b37a348d10f
Best Damn Cat <bestdamncat@gmail.com>
2024-05-22 21:13:02 -0700

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faTelegram } from '@fortawesome/free-brands-svg-icons';
-import rigoImage from "../../img/rigo-baby.png";
+import { faTwitter, faTelegram } from '@fortawesome/free-brands-svg-icons'; 
+
+import bbImage from "../../img/buckbundy.gif";
 
 const Home = () => {
   return (
     <div className="text-center">
-      <h1 className="text-center mt-5">The Purr-fect Revolution</h1>
-      <h3>Unleash Your Inner Crypto-Cat with the Best Damn Cat Token!</h3>
+      <h1 className="text-center mt-5">Buck Bundy</h1>
+      <h3>I may be just a dog, but even I know that money is like a bone buried in the backyard: hard to find and never enough to go around!</h3>
 
-      <h5>CA: <a href="https://www.pump.fun/Gm8ZAUfqS1qr2PbXKceLhiTEzq9DzQmcfuWMX9MRqPS3">Gm8ZAUfqS1qr2PbXKceLhiTEzq9DzQmcfuWMX9MRqPS3</a></h5>
+      <h5>CA: <a href="https://www.pump.fun/"></a></h5>
       <p>
-        <a href="https://twitter.com/bestdamncat" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
+        <a href="https://twitter.com/buckbundysol" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
           <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '1.2em', marginRight: '5px' }} />
         </a>	
 			
-        <a href="https://t.me/bestdamncat" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
+        <a href="https://t.me/buckbundysol" style={{ fontFamily: 'Arial, sans-serif', color: '#0088cc', textDecoration: 'none' }}>
           <FontAwesomeIcon icon={faTelegram} style={{ fontSize: '1.2em', marginRight: '5px' }} />
         </a>
 
       </p>			
       <p>
-        <img src={rigoImage} />
+        <img src={bbImage} />
       </p>
     </div>
   );
